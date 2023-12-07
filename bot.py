349f5e57b1b83afc5d0ec878ba2abd3268a057db
Pavel Shyliahau <pashxa@syntronic.com>
2023-12-07 16:30:20 -0500

    Fix for new version

--- bot.py
+++ bot.py
@@ -1,28 +1,31 @@
+import random
+from telegram.ext import Application, CommandHandler, ContextTypes, filters
+from telegram import Update
+import os
 from dotenv import load_dotenv
 load_dotenv()
-import os
-from telegram.ext import Updater, CommandHandler, Filters
-import random
-
-TOKEN = os.getenv('TELEGRAM_TOKEN')
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
 
 # Dictionary to store user data
 user_data = {}
 
-# Command to start registration
-def register(update, context):
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Hello! I am your Secret Santa bot.')
+
+
+async def register(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.message.from_user.id
     user_info = ' '.join(context.args)
     if ':' not in user_info:
-        update.message.reply_text("Please use ':' to separate your name and address.")
+        update.message.reply_text(
+            "Please use ':' to separate your name and address.")
         return
     user_data[user_id] = user_info
-    update.message.reply_text("You're registered! Wait for your Secret Santa match.")
-    
-# Command to start Secret Santa
-def secret_santa(update, context):
+    update.message.reply_text(
+        "You're registered! Wait for your Secret Santa match.")
+
+
+async def secret_santa(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.effective_chat.id
     user_ids = list(user_data.keys())
 
@@ -41,13 +44,19 @@ def secret_santa(update, context):
     for i in range(len(user_ids)):
         giver_id = user_ids[i]
         receiver_id = user_ids[(i + 1) % len(user_ids)]
-        context.bot.send_message(giver_id, f"Your Secret Santa match: {user_data[receiver_id]}")
+        context.bot.send_message(
+            giver_id, f"Your Secret Santa match: {user_data[receiver_id]}")
 
     update.message.reply_text("Secret Santa matches have been sent!")
 
-# Adding handlers
-dispatcher.add_handler(CommandHandler('register', register, Filters.chat_type.groups))
-dispatcher.add_handler(CommandHandler('secretsanta', secret_santa, Filters.chat_type.groups))
+if __name__ == '__main__':
+    application = Application.builder().token(os.getenv('TELEGRAM_TOKEN')).build()
 
-# Start the bot
-updater.start_polling()
+    application.add_handler(CommandHandler(
+        'start', start, filters.ChatType.GROUP))
+    application.add_handler(CommandHandler(
+        'register', register, filters.ChatType.GROUP))
+    application.add_handler(CommandHandler(
+        'secretsanta', secret_santa, filters.ChatType.GROUP))
+
+    application.run_polling()
